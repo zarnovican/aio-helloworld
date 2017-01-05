abbd34944d51c5d70a7eba11382c355b5c664311
Brano Zarnovican <zarnovican@gmail.com>
2017-01-05 18:23:56 +0100

    dummy argparse with prog description

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,5 +1,18 @@
+"""
+Template for aio http service
+
+Here is a long description.
+Multi-lined, etc..
+"""
+import argparse
 import sys
+
+from . import SERVICE_NAME
 
 
 def main():
-    print(sys.argv)
+    parser = argparse.ArgumentParser(
+                prog=SERVICE_NAME, description=__doc__,
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+             )
+    args = parser.parse_args()
