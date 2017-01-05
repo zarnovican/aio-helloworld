6a7074dc3fc903c52970aefce2252a53e4a38343
Brano Zarnovican <zarnovican@gmail.com>
2017-01-05 19:14:42 +0100

    setup basic logging to console or journal

--- helloworld/main.py
+++ helloworld/main.py
@@ -5,9 +5,34 @@ Here is a long description.
 Multi-lined, etc..
 """
 import argparse
+import logging
+import logging.handlers
+import os
 import sys
 
 from . import SERVICE_NAME
+
+
+def setup_logging(args):
+    """Configure 'logging' handlers/formatters and log verbosity"""
+
+    if os.path.exists('/dev/log') and args.log == 'auto':
+        print('Logging is redirected to systemd journal. Tail with "journalctl -t {} -f"'.format(SERVICE_NAME))
+        handler = logging.handlers.SysLogHandler(address='/dev/log')
+        formatter = logging.Formatter('{}: %(name)s %(message)s'.format(SERVICE_NAME))
+    else:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+
+    if args.verbosity >= 2:
+        logger.setLevel(logging.DEBUG)
+    elif args.verbosity >= 1:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.WARN)
 
 
 def main():
@@ -15,4 +40,11 @@ def main():
                 prog=SERVICE_NAME, description=__doc__,
                 formatter_class=argparse.RawDescriptionHelpFormatter,
              )
+    parser.add_argument('--log', default='auto', metavar='auto|console',
+        help='"auto" will use journal if available. '\
+            'Otherwise, we will use console (default: %(default)s)')
+    parser.add_argument('-v', '--verbosity', action='count',
+        help='increase output verbosity (-v for INFO, -vv for DEBUG)')
     args = parser.parse_args()
+
+    setup_logging(args)
